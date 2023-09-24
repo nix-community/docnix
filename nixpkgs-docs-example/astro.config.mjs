dee9a0c8941de186169d30aff472f9133031928d
hsjobeki <hsjobeki+github@gmail.com>
2023-09-24 21:51:16 +0200

    omfg

--- nixpkgs-docs-example/astro.config.mjs
+++ nixpkgs-docs-example/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hsjobeki.github.io",
-  base: "/nix-docs-comments",
+  base: "/nix-doc-comments",
   integrations: [
     starlight({
       title: "nixos/nixpkgs reference docs",
