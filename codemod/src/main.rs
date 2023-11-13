eb2c7d9eab7c6a85cfc1c236697565baa961724c
hsjobeki <hsjobeki+github@gmail.com>
2023-11-13 15:04:07 +0100

    add support for argument docs migration

--- codemod/src/main.rs
+++ codemod/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use rnix::ast::{AstToken, AttrpathValue, Comment, Expr, Lambda, Param};
 use rnix::{SyntaxKind, SyntaxNode, SyntaxToken};
 use rowan::{ast::AstNode, GreenToken, NodeOrToken, WalkEvent};
 use std::fs::File;
@@ -6,10 +7,146 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::println;
 use std::{env, fs};
+use textwrap::dedent;
 use walkdir::WalkDir;
 
 const EXAMPLE_LANG: &str = "nix";
 const TYPE_LANG: &str = "";
+
+/// Represent a single function argument name and its (optional)
+/// doc-string.
+#[derive(Clone, Debug)]
+pub struct SingleArg {
+    pub name: String,
+    pub doc: Option<String>,
+}
+
+/// Represent a function argument, which is either a flat identifier
+/// or a pattern set.
+#[derive(Clone, Debug)]
+pub enum Argument {
+    /// Flat function argument (e.g. `n: n * 2`).
+    Flat(SingleArg),
+
+    /// Pattern function argument (e.g. `{ name, age }: ...`)
+    Pattern(Vec<SingleArg>),
+}
+
+///
+fn handle_indentation(raw: &str) -> Option<String> {
+    let result: String = match raw.split_once('\n') {
+        Some((first, rest)) => {
+            format!("{}\n{}", first.trim_start(), dedent(rest))
+        }
+        None => raw.into(),
+    };
+
+    Some(result.trim().to_owned()).filter(|s| !s.is_empty())
+}
+
+/// Retrieve documentation comments.
+fn retrieve_doc_comment(node: &SyntaxNode, allow_line_comments: bool) -> Option<String> {
+    // if the current node has a doc comment it'll be immediately preceded by that comment,
+    // or there will be a whitespace token and *then* the comment tokens before it. We merge
+    // multiple line comments into one large comment if they are on adjacent lines for
+    // documentation simplicity.
+    let mut token = node.first_token()?.prev_token()?;
+    if token.kind() == SyntaxKind::TOKEN_WHITESPACE {
+        token = token.prev_token()?;
+    }
+    if token.kind() != SyntaxKind::TOKEN_COMMENT {
+        return None;
+    }
+
+    // if we want to ignore line comments (eg because they may contain deprecation
+    // comments on attributes) we'll backtrack to the first preceding multiline comment.
+    while !allow_line_comments && token.text().starts_with('#') {
+        token = token.prev_token()?;
+        if token.kind() == SyntaxKind::TOKEN_WHITESPACE {
+            token = token.prev_token()?;
+        }
+        if token.kind() != SyntaxKind::TOKEN_COMMENT {
+            return None;
+        }
+    }
+
+    if token.text().starts_with("/*") {
+        return Some(Comment::cast(token)?.text().to_string());
+    }
+
+    // backtrack to the start of the doc comment, allowing only adjacent line comments.
+    // we don't care much about optimization here, doc comments aren't long enough for that.
+    if token.text().starts_with('#') {
+        let mut result: String = String::new();
+        while let Some(comment) = Comment::cast(token) {
+            if !comment.syntax().text().starts_with('#') {
+                break;
+            }
+            result.insert_str(0, comment.text().trim());
+            let ws = match comment.syntax().prev_token() {
+                Some(t) if t.kind() == SyntaxKind::TOKEN_WHITESPACE => t,
+                _ => break,
+            };
+            // only adjacent lines continue a doc comment, empty lines do not.
+            match ws.text().strip_prefix('\n') {
+                Some(trail) if !trail.contains('\n') => result.insert(0, ' '),
+                _ => break,
+            }
+            token = match ws.prev_token() {
+                Some(c) => c,
+                _ => break,
+            };
+        }
+        return Some(result);
+    }
+
+    None
+}
+
+/// Copied from nixdoc.
+/// Traverse a Nix lambda and collect the identifiers of arguments
+/// until an unexpected AST node is encountered.
+fn collect_lambda_args(mut lambda: Lambda) -> Vec<Argument> {
+    let mut args = vec![];
+
+    loop {
+        match lambda.param().unwrap() {
+            // a variable, e.g. `id = x: x`
+            Param::IdentParam(id) => {
+                args.push(Argument::Flat(SingleArg {
+                    name: id.to_string(),
+                    // doc:
+                    doc: handle_indentation(
+                        &retrieve_doc_comment(id.syntax(), true).unwrap_or_default(),
+                    ),
+                }));
+            }
+            // an attribute set, e.g. `foo = { a }: a`
+            Param::Pattern(pat) => {
+                // collect doc-comments for each attribute in the set
+                let pattern_vec: Vec<_> = pat
+                    .pat_entries()
+                    .map(|entry| SingleArg {
+                        name: entry.ident().unwrap().to_string(),
+                        doc: None, // handle_indentation(
+                                   //     &retrieve_doc_comment(entry.syntax(), true).unwrap_or_default(),
+                                   // ),
+                    })
+                    .collect();
+
+                args.push(Argument::Pattern(pattern_vec));
+            }
+        }
+
+        // Curried or not?
+        match lambda.body() {
+            Some(Expr::Lambda(inner)) => lambda = inner,
+            _ => break,
+        }
+    }
+
+    args
+}
 
 fn parse_doc_comment(raw: &str, indent: usize) -> String {
     enum ParseState {
@@ -92,9 +229,57 @@ fn parse_doc_comment(raw: &str, indent: usize) -> String {
     markdown
 }
 
-fn format_comment(text: &str, prev: Option<&SyntaxToken>) -> String {
+fn get_argument_docs(token: &SyntaxToken, ident: &str) -> Option<String> {
+    let mut step = token.next_sibling_or_token();
+
+    // Find the Expr that is a lambda.
+    let doc_expr = loop {
+        if step.is_none() {
+            // If there is no next token or node
+            break None;
+        } else if let Some(NodeOrToken::Node(ref node)) = step {
+            match node.kind() {
+                // SyntaxKind::NODE_LAMBDA => break Some(node.clone()),
+                SyntaxKind::NODE_ATTRPATH_VALUE => {
+                    if let Some(value) = AttrpathValue::cast(node.clone()) {
+                        break value.value();
+                    } else {
+                        break None;
+                    }
+                }
+                _ => {}
+            };
+        } else {
+        }
+        step = step.unwrap().next_sibling_or_token();
+    };
+
+    let mut argument_docs: Option<String> = None;
+    if let Some(Expr::Lambda(l)) = doc_expr {
+        let args = collect_lambda_args(l);
+        let mut docs = String::new();
+        for arg in args {
+            match arg {
+                Argument::Flat(single_arg) => {
+                    docs.push_str(&format!(
+                        "{ident}- [{}] {}\n",
+                        single_arg.name,
+                        single_arg.doc.unwrap_or(String::from(""))
+                    ));
+                }
+                Argument::Pattern(_pattern) => (),
+            }
+        }
+        argument_docs = Some(docs);
+    }
+    return argument_docs;
+}
+
+fn format_comment(text: &str, token: &SyntaxToken) -> String {
     let content = text.strip_prefix("/*").unwrap().strip_suffix("*/").unwrap();
     let mut whitespace = "";
+    let prev = &token.prev_token();
+
     if let Some(prev) = prev {
         whitespace = prev.text();
     }
@@ -108,7 +293,12 @@ fn format_comment(text: &str, prev: Option<&SyntaxToken>) -> String {
         .lines()
         .map(|content| format!("{}{}", indent_2, content))
         .collect();
-    let markdown = parse_doc_comment(&lines.join("\n"), indentation + 2);
+    let mut markdown = parse_doc_comment(&lines.join("\n"), indentation + 2);
+
+    if let Some(argument_docs) = get_argument_docs(token, &indent_2) {
+        markdown.push_str(&format!("\n\n{indent_2}# Arguments"));
+        markdown.push_str(&format!("\n\n{argument_docs}"));
+    }
 
     return format!("/**\n{}\n{}*/", markdown, indent_1);
 }
@@ -170,11 +360,12 @@ fn replace_first_comment(syntax: &SyntaxNode) -> Option<SyntaxNode> {
                 NodeOrToken::Token(token) => match token.kind() {
                     SyntaxKind::TOKEN_COMMENT => {
                         if token.text().starts_with("/**") || token.text().starts_with("#") {
+                            // Already a doc-comment or not supposed to be migrated
                             continue;
                         }
                         let replacement: GreenToken = GreenToken::new(
                             rowan::SyntaxKind(token.kind() as u16),
-                            &format_comment(token.text(), token.prev_token().as_ref()),
+                            &format_comment(token.text(), &token),
                         );
                         let green = token.replace_with(replacement);
                         let updated = syntax.replace_with(green);
@@ -233,6 +424,7 @@ fn modify_file(file_path: PathBuf) {
         return;
     }
 
+    println!("Syntax \n {:?}", root.clone().unwrap().syntax().clone());
     let syntax = root.unwrap().syntax().clone_for_update();
     let mut maybe_replaced = replace_first_comment(&syntax);
     let mut count = 0;
